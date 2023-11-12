5ad3b54238a7ce23f264471026cc15e8e5dd3888
Jean Coelho <jeancoelhoneves8@gmail.com>
2023-11-12 02:08:28 -0300

    implementando novo Quem Somos, ajustes gerais

--- script/scroll.js
+++ script/scroll.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
         // Subtrai a altura do cabeçalho fixo e um valor adicional de margem
         const headerHeight = $('header').outerHeight();
-        const margin = 25; // Valor adicional de margem (ajuste conforme necessário)
+        const margin = 30; // Valor adicional de margem (ajuste conforme necessário)
 
         $('html, body').animate({
             scrollTop: targetPosition - headerHeight - margin
