fc354c46feacd3616b2f0ab5e039279dbaea9348
Jean Coelho <jeancoelhoneves8@gmail.com>
2023-11-13 15:43:11 -0300

    mudança no qrcode home

--- php/protectAdm.php
+++ php/protectAdm.php
@@ -5,7 +5,24 @@ if (!isset($_SESSION)) {
 }
 
 if (empty($_SESSION["controleAdm"]) || $_SESSION["controleAdm"] !== "logado") {
-    die("Você não pode acessar essa pagina porque não esta logado. <p><a href=\"/inovafin_site/index.html\"> Voltar </a></p>");
+    $error_message = "Você não pode acessar essa página porque não está logado.";
+    echo "
+        <style>
+            a:focus {
+                outline: none !important;
+            }
+        </style>
+        <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Você precisa fazer login antes de acessar esta página.',
+                showConfirmButton: false,
+                footer: '<a href=\"/inovafin_site/index.html\">Voltar</a>'
+            }).then(function() {
+                window.location.href = '/inovafin_site/index.html';
+            });
+          </script>";
+    exit;
 }
-
 ?>
